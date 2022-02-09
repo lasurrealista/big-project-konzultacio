d41e9f6e6a65feb696913c43719f69a0c6c794c0
lasurrealista <68642008+lasurrealista@users.noreply.github.com>
2022-02-09 10:47:55 +0100

    Initial commit

---
+++ webshop/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { HomeComponent } from './page/home/home.component';
+import { HeaderComponent } from './common/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
